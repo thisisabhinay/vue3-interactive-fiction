5f84851359e935bb8dae5257d80dc952af836415
thisisabhinay <a8h1nay@gmail.com>
2023-11-04 14:24:53 +0530

    feat: create components to expand and describe scenes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 import TheWelcome from "./the-welcome.vue"
+import SceneExpander from "./scene-expander.vue"
+import SceneDescription from "./scene-description.vue"
 
-export { TheWelcome }
+export { TheWelcome, SceneExpander, SceneDescription }
---
+++ src/components/scene-description.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  narration: boolean
+}>()
+</script>
+
+<template>
+  <p class="py-3 text-base leading-6" :class="narration ? 'italic' : ''">
+    <slot />
+  </p>
+</template>
+
+<style scoped></style>
