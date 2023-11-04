48267cc238b272606e036a959060c9f012b8aaf5
thisisabhinay <a8h1nay@gmail.com>
2023-11-04 18:04:05 +0530

    fix: add cover image

--- src/components/the-welcome.vue
+++ src/components/the-welcome.vue
@@ -3,7 +3,12 @@ import { RouterLink } from "vue-router"
 </script>
 
 <template>
-  <section id="TheWelcome" class="flex flex-col items-center justify-center gap-10 py-20">
+  <section id="TheWelcome" class="flex flex-col items-center justify-center gap-5 py-3">
+    <img
+      src="/images/cover.jpeg"
+      alt="The Second Moon cover image showing a night time city skyline with two moons in pixel art style"
+      class="w-full h-auto"
+    />
     <h1 class="text-7xl font-bold text-center">The Second Moon</h1>
     <RouterLink
       to="/game"
