aad4191ceefb256b7977220a83bbdb269ba865cd
thisisabhinay <a8h1nay@gmail.com>
2023-11-10 01:29:52 +0530

    feat: create on-scroll parallax effect

--- src/interactions/paw-up.vue
+++ src/interactions/paw-up.vue
@@ -1,25 +1,52 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, onUnmounted, onMounted } from "vue"
 const el = ref(null)
 
 const emit = defineEmits<{
   (e: "flag", value: boolean): void
 }>()
 
-function mouseMoveHandler(event: any) {
-  // const e = el.value
-  // console.log(e, e.firstElementChild)
-  // console.log(event)
+function parallax(event) {
+  window.requestAnimationFrame(function () {
+    var currentElement = el.value
+    var scrolled = window.scrollY
+    console.log(event, currentElement, scrolled, window)
+    // currentElement.style.transform = "translate3d(0," + scrolled * 0.3 + "px, 0)"
+    currentElement.style.backgroundPositionY = "-" + (400 - scrolled) + "px"
+  })
 }
+
+onMounted(() => {
+  window.addEventListener("scroll", parallax)
+})
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", parallax)
+})
 </script>
 
 <template>
-  <div id="InteractionPawUp" @mousemove="mouseMoveHandler" ref="el">
-    <img
-      src="/images/up-paw.png"
-      alt="A cat walking alongside you, almost as if it's attempting to guide you."
-      class="w-full h-auto mx-auto"
-      @click="() => emit('flag', true)"
-    />
+  <div id="InteractionPawUp" @mousemove="mouseMoveHandler" class="parallax-container">
+    <div class="parallax" @click="() => emit('flag', true)" ref="el" />
   </div>
 </template>
+
+<style>
+.parallax-container {
+  position: relative;
+  height: 400px;
+  overflow: hidden;
+}
+
+.parallax {
+  background: url("/images/up-paw-full.png");
+  background-size: cover;
+  background-position-x: center;
+  background-position-y: -400px;
+  background-repeat: no-repeat;
+  position: absolute;
+  height: 200%;
+  width: 100%;
+  z-index: -1;
+}
+</style>
