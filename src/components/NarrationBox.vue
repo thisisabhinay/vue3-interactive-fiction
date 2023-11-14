ce80676ac111ae916546923c17319254a3520dba
thisisabhinay <a8h1nay@gmail.com>
2023-11-15 00:14:22 +0530

    feat: create component to display narration

---
+++ src/components/NarrationBox.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  data: any
+}>()
+</script>
+
+<template>
+  <div
+    id="Narration"
+    class="grid grid-cols-1 gap-4 border-solid border-t border-b py-4 border-gray-700 italic text-gray-500 text-center"
+  >
+    <template v-for="(text, index) in data.text" :key="index">
+      {{ text }}
+    </template>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,17 @@ import SceneExpander from "./scene-expander.vue"
 import SceneLoader from "./scene-loader.vue"
 import SceneDescription from "./scene-description.vue"
 import BracketLine from "./bracket-line.vue"
+import DialogueMC from "./DialogueMC.vue"
+import DialogueOC from "./DialogueOC.vue"
+import NarrationBox from "./NarrationBox.vue"
 
-export { TheWelcome, SceneExpander, SceneLoader, SceneDescription, BracketLine }
+export {
+  TheWelcome,
+  SceneExpander,
+  SceneLoader,
+  SceneDescription,
+  BracketLine,
+  DialogueMC,
+  DialogueOC,
+  NarrationBox
+}
