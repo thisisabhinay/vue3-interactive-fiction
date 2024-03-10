cca8f275b37d9c9515756d339e3dad4e5eb43fe3
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-03-10 14:06:46 +0530

    feat: create constants file for player actions

---
+++ src/constants/player-actions.ts
@@ -0,0 +1,49 @@
+const actions = [
+  {
+    name: "Give",
+    key: "action-give",
+    color: "lime-500"
+  },
+  {
+    name: "Open",
+    key: "action-open",
+    color: "yellow-500"
+  },
+  {
+    name: "Close",
+    key: "action-close",
+    color: "pink-500"
+  },
+  {
+    name: "Pick up",
+    key: "action-pick-up",
+    color: "teal-500"
+  },
+  {
+    name: "Look at",
+    key: "action-look-at",
+    color: "blue-500"
+  },
+  {
+    name: "Talk to",
+    key: "action-talk-to",
+    color: "amber-500"
+  },
+  {
+    name: "Use",
+    key: "action-use",
+    color: "indigo-500"
+  },
+  {
+    name: "Push",
+    key: "action-push",
+    color: "red-500"
+  },
+  {
+    name: "Pull",
+    key: "action-pull",
+    color: "purple-500"
+  }
+]
+
+export default actions
