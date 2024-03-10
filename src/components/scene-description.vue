f10f48bf687519e1e0d1e0f19f8e0bb5a3c8f4e7
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-03-10 22:11:52 +0530

    fix: showing interactive text and triggering action based on specific player action

--- src/components/scene-description.vue
+++ src/components/scene-description.vue
@@ -1,22 +1,36 @@
 <script setup lang="ts">
-import generateInteractiveText from "@/utils/generate-interactive-text"
+import generateInteractiveDOMText from "@/utils/generate-interactive-dom-text"
 import { type Interaction } from "@/types/interaction"
-import { ref, onMounted } from "vue"
+import { SCENE_OG_HTML_ATTR } from "@/constants/selectors"
+import { ref, onMounted, inject, watch } from "vue"
+
+const { playerAction } = inject<any>("player-action")
 
 const props = defineProps<{
   interactions?: Interaction[]
 }>()
 
+const emit = defineEmits<{
+  (e: "DOMUpdated"): void
+}>()
+
 const container = ref<Element>()
 
-function updateMarkup() {
+function updateMarkup(action: string) {
   if (!props.interactions) return
 
-  const result = generateInteractiveText(container, props.interactions)
+  const result = generateInteractiveDOMText(container, props.interactions, action)
   container.value ? (container.value.innerHTML = result as string) : null
+
+  emit("DOMUpdated")
 }
 
-onMounted(updateMarkup)
+onMounted(() => {
+  // Persisting the original content as DOM state
+  container.value?.setAttribute(SCENE_OG_HTML_ATTR, container.value?.innerHTML)
+  updateMarkup(playerAction.value)
+})
+watch(playerAction, (action: string) => updateMarkup(action))
 </script>
 
 <template>
--- src/scenes/scene-room-1-0.vue
+++ src/scenes/scene-room-1-0.vue
@@ -7,16 +7,45 @@ import ListChoices from "@/components/list-choices.vue"
 import SceneRoom1_1 from "@/scenes/scene-room-1-1.vue"
 import SceneRoom1_2 from "@/scenes/scene-room-1-2.vue"
 import SceneRoom1_3 from "@/scenes/scene-room-1-3.vue"
+import { INTERACTIVE_SELECTOR } from "@/constants/selectors"
+import { watch, inject } from "vue"
 
 const storyChoices: ListChoice[] = [
   { text: "Attempt to move: (Test the physical constraints of this world)", scene: SceneRoom1_1 },
   { text: "'Who are you?'': (Seek information on the voice and this place)", scene: SceneRoom1_2 },
   { text: "Focus on the voice: (Try to discern a location or source)", scene: SceneRoom1_3 }
 ]
+
+const { playerAction } = inject<any>("player-action")
+
+function triggerInteraction(event: Event) {
+  console.log(event)
+  const el = event.target as HTMLElement
+  const targetName = el.innerText
+  const interaction = playerInteractionsOpeningRoom?.find(
+    (interaction) => interaction.target === targetName
+  )
+
+  console.log(targetName, interaction[playerAction.value])
+}
+
+function attachEventListeners() {
+  if (!playerInteractionsOpeningRoom || !playerInteractionsOpeningRoom?.length) return
+
+  const interactiveItems = document.querySelectorAll(INTERACTIVE_SELECTOR)
+  Array.from(interactiveItems, (item) => {
+    item.addEventListener("click", triggerInteraction)
+  })
+}
+
+watch(playerAction, attachEventListeners)
 </script>
 
 <template>
-  <SceneDescription :interactions="playerInteractionsOpeningRoom">
+  <SceneDescription
+    :interactions="playerInteractionsOpeningRoom"
+    @DOMUpdated="attachEventListeners"
+  >
     <p class="first-letter:text-4xl first-letter:font-bold">
       The world is a blur. Colors swirl and drip like melting wax over your vision. You try to speak
       but only a croaking whisper escapes. A prickling tingle fills your limbs. Are you asleep?
