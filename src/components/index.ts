7e78d85aa993c9b543ae0631f1ae73178634a0de
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-03-10 11:14:43 +0530

    chore: remove components index

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import TheWelcome from "./the-welcome.vue"
-import GameRoom from "./game-room.vue"
-import SceneLoader from "./scene-loader.vue"
-import SceneDescription from "./scene-description.vue"
-
-export { TheWelcome, GameRoom, SceneLoader, SceneDescription }
