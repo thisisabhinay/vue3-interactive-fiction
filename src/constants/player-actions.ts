3147dd0ad9a84e66918a685af7dae83e5e011d54
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-03-11 14:35:38 +0530

    feat: define enum for player actions

--- src/constants/player-actions.ts
+++ src/constants/player-actions.ts
@@ -1,52 +1,65 @@
+export enum gameAction {
+  GIVE = "action-give",
+  OPEN = "action-open",
+  CLOSE = "action-close",
+  PICK_UP = "action-pick-up",
+  LOOK_AT = "action-look-at",
+  TALK_TO = "action-talk-to",
+  USE = "action-use",
+  PUSH = "action-push",
+  PULL = "action-pull",
+  TOUCH = "action-touch"
+}
+
 export const actions = [
   {
     name: "Give",
-    key: "action-give",
+    key: gameAction.GIVE,
     color: "lime-500"
   },
   {
     name: "Open",
-    key: "action-open",
+    key: gameAction.OPEN,
     color: "yellow-500"
   },
   {
     name: "Close",
-    key: "action-close",
+    key: gameAction.CLOSE,
     color: "pink-500"
   },
   {
     name: "Pick up",
-    key: "action-pick-up",
+    key: gameAction.PICK_UP,
     color: "teal-500"
   },
   {
     name: "Look at",
-    key: "action-look-at",
+    key: gameAction.LOOK_AT,
     color: "blue-500"
   },
   {
     name: "Talk to",
-    key: "action-talk-to",
+    key: gameAction.TALK_TO,
     color: "amber-500"
   },
   {
     name: "Use",
-    key: "action-use",
+    key: gameAction.USE,
     color: "indigo-500"
   },
   {
     name: "Push",
-    key: "action-push",
+    key: gameAction.PUSH,
     color: "red-500"
   },
   {
     name: "Pull",
-    key: "action-pull",
+    key: gameAction.PULL,
     color: "purple-500"
   },
   {
     name: "Touch",
-    key: "action-touch",
+    key: gameAction.TOUCH,
     color: "rose-400"
   }
 ]
